1a1a9651c5f5f7fe69b1bc19728a63c231767f91
Thai Tran <thaitd1985@gmail.com>
2020-02-08 18:48:01 +0800

    exception handle

--- paranuara/apis.py
+++ paranuara/apis.py
@@ -27,11 +27,40 @@ def company_detail(request, pk):
     """
     try:
         company = Company.objects.get(pk=pk)
+
     except Company.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        content = {"status": "Fail", "error": "Company does not exist!"}
+        return Response(content, status=status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         serializer = serializers.CompanyDetailSerializer(company)
-        return Response(serializer.data)
+        content = {"status": "Success", "data": serializer.data}
+        return Response(content, status=status.HTTP_200_OK)
+
+
+def get_friends_in_common(ids, eye_color, has_died):
+    list_ids = ids.split(",")
+    # For this requirement only accept 2 people
+    if len(list_ids) < 2:
+        content = {"status": "Fail", "error": "Only accept 2 people!"}
+        return Response(content, status=status.HTTP_204_NO_CONTENT)
+
+    friend_list_people_1 = Friend.objects.filter(people_index__index=list_ids[0]).values_list("friend_index__index",
+                                                                                              flat=True)
+    friend_list_people_2 = Friend.objects.filter(people_index__index=list_ids[1]).values_list("friend_index__index",
+                                                                                              flat=True)
+
+    common_friend_ids = set(friend_list_people_1).intersection(set(friend_list_people_2))
+    friends = People.objects.filter(index__in=common_friend_ids, eye_color=eye_color, has_died=has_died)
+    response_common_friends = serializers.FriendSerializer(data=friends, many=True)
+    response_common_friends.is_valid()
+
+    peoples = People.objects.filter(index__in=list_ids[0:2]).all()
+    response_peoples = serializers.PeopleSerializerSimple(data=peoples, many=True)
+    response_peoples.is_valid()
+
+    content = {"status": "Success", "data": {"peoples": response_peoples.data,
+                                             "common_friends": response_common_friends.data}}
+    return content
 
 
 @api_view(['GET'])
@@ -45,27 +74,12 @@ def people_friends_in_common(request):
     eye_color = request.query_params.get('eye_color', "brown")
     has_died = request.query_params.get('has_died', False)
     if request.method == 'GET':
-        list_ids = ids.split(",")
-        # For this requirement only accept 2 people
-        if len(list_ids) < 2:
-            return Response(status=status.HTTP_204_NO_CONTENT)
-
-        friend_list_people_1 = Friend.objects.filter(people_index__index=list_ids[0]).values_list("friend_index__index",
-                                                                                                  flat=True)
-        friend_list_people_2 = Friend.objects.filter(people_index__index=list_ids[1]).values_list("friend_index__index",
-                                                                                                  flat=True)
-
-        common_friend_ids = set(friend_list_people_1).intersection(set(friend_list_people_2))
-        friends = People.objects.filter(index__in=common_friend_ids, eye_color=eye_color, has_died=has_died)
-        response_common_friends = serializers.FriendSerializer(data=friends, many=True)
-        response_common_friends.is_valid()
-
-        peoples = People.objects.filter(index__in=list_ids[0:2]).all()
-        response_peoples = serializers.PeopleSerializerSimple(data=peoples, many=True)
-        response_peoples.is_valid()
-
-        return Response(
-            {"peoples": response_peoples.data, "common_friends": response_common_friends.data})
+        try:
+            content = get_friends_in_common(ids, eye_color, has_died)
+        except Exception:
+            content = {"status": "Fail", "error": "Bad request!"}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+        return Response(content, status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
@@ -78,7 +92,9 @@ def people_detail(request, pk):
     try:
         people = People.objects.get(pk=pk)
     except People.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        content = {"status": "Fail", "error": "People not found!"}
+        return Response(content, status=status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         serializer = serializers.PeopleSerializerDetail(people)
-        return Response(serializer.data)
+        content = {"status": "Success", "data": serializer.data}
+        return Response(content, status=status.HTTP_200_OK)
